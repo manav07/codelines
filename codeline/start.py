a6816209254f1cb495a30260b974c0b0d240e126
manav07 <manavagrawall123@gmail.com>
2023-12-17 23:54:16 +0530

    lint fixes

--- codeline/start.py
+++ codeline/start.py
@@ -5,16 +5,16 @@ class adapter:
 
     def __init__(self, file_type):
         self.file_type = file_type
-        self.instance = {
-            "py": pythonFileProcessing(),
-            "java": pythonFileProcessing(),
-            "ts": pythonFileProcessing(),
-            "js": pythonFileProcessing()
-        }
 
 
     def get(self):
-        return self.instance[self.file_type] if self.file_type in self.instance else None
+        instance = {
+            "py": pythonFileProcessing(),
+            "java": javaFileProcessing(),
+            "ts": tsFileProcessing(),
+            "js": jsFileProcessing()
+        }
+        return instance[self.file_type] if self.file_type in instance else None
 
 
 class pythonFileProcessing:
@@ -32,15 +32,11 @@ class pythonFileProcessing:
             if not line:
                 resp.blankLines += 1
             else:
-                if line[0] == "#":
+                if line.startswith("#"):
                     resp.commentedLines += 1
                 else:
                     resp.codeLines += 1
         return resp
-
-    def get_multiline_comment_count(self,lines):
-        pass
-
 
 
 class jsFileProcessing:
@@ -79,4 +75,4 @@ for root, dirs, files in os.walk(dir_path):
             resp = file_obj.read_file(lines)
             print(f"for file:{file} \n {json.dumps(resp.__dict__)}")
         except:
-            print("extesion is not handled for file : {file}")
+            print(f"extesion is not handled for file : {file}")
